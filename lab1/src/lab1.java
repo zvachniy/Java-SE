91c8952e60ca951201ac46cdf8eb4418d5a2abd1
zvachniy <80152850+zvachniy@users.noreply.github.com>
2021-03-14 16:38:39 +0300

    Выдает Hello World! сто раз

--- lab1/src/lab1.java
+++ lab1/src/lab1.java
@@ -1,5 +1,9 @@
 public class lab1 {
-  public static void main(String[] args) {
-    System.out.print("Hello World!");
-  }
+    public static void main(String[] args) {
+        int n = 0;
+        while (n < 100) {
+            n++;
+            System.out.println("Hello World! " + n);
+        }
+    }
 }
