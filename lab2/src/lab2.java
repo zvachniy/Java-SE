37caa2c87f53d41f6ffe5e2d0d48eeb0cfa5a160
zvachniy <80152850+zvachniy@users.noreply.github.com>
2021-03-14 17:51:19 +0300

    форматирование

--- lab2/src/lab2.java
+++ lab2/src/lab2.java
@@ -8,31 +8,16 @@ public class lab2 {
         System.out.println("Понедельник - 1, Вторник - 2 и т.д.");
         System.out.print("Введите номер дня: ");
         int num = in.nextInt();
-        switch (num) {   //не забывай про break
-            default:
-                System.out.println("Error");
-                break;
-            case 1:
-                System.out.println(arrayDays[0]);
-                break;
-            case 2:
-                System.out.println(arrayDays[1]);
-                break;
-            case 3:
-                System.out.println(arrayDays[2]);
-                break;
-            case 4:
-                System.out.println(arrayDays[3]);
-                break;
-            case 5:
-                System.out.println(arrayDays[4]);
-                break;
-            case 6:
-                System.out.println(arrayDays[5]);
-                break;
-            case 7:
-                System.out.println(arrayDays[6]);
-                break;
+        //не забывай про break
+        switch (num) {
+            default -> System.out.println("Error");
+            case 1 -> System.out.println(arrayDays[0]);
+            case 2 -> System.out.println(arrayDays[1]);
+            case 3 -> System.out.println(arrayDays[2]);
+            case 4 -> System.out.println(arrayDays[3]);
+            case 5 -> System.out.println(arrayDays[4]);
+            case 6 -> System.out.println(arrayDays[5]);
+            case 7 -> System.out.println(arrayDays[6]);
         }
     }
 }
