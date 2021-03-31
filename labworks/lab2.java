1c70f6fd3103db6f7418d7dabfa6c01c4b3e56b9
d.kachalov <d.kachalov@exporesource.ru>
2021-03-31 17:23:05 +0300

    лаба2 Алфавит

---
+++ labworks/lab2.java
@@ -0,0 +1,9 @@
+public class lab2 {
+    public static void main(String[] args) {
+        char Letter = 'A';
+        for (int i = 1; i <= 26; i++) {
+            System.out.print(Letter);
+            Letter++;
+        }
+    }
+}
