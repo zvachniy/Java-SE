df87d8881a581828fd16a04371b765e90c68fd69
d.kachalov <d.kachalov@exporesource.ru>
2021-04-05 12:05:44 +0300

    совершенные числа, калька с задания c++

---
+++ labworks/lab4.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class lab4 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);    //инициализируется для приблыды ввода консоли
+
+    }
+}
