5876c7abc580f541ebe4f02446e4289a91abd319
d.kachalov <d.kachalov@exporesource.ru>
2021-04-08 10:37:43 +0300

    добавление j55

---
+++ labworks/spbu/works/j55/MyInt.java
@@ -0,0 +1,5 @@
+package spbu.works.j55;
+
+public class MyInt {
+    public int value;
+}
---
+++ labworks/spbu/works/j55/test.java
@@ -0,0 +1,16 @@
+package spbu.works.j55;
+
+public class test {
+/*
+    static void change(int t){
+        20
+    }
+
+    public static void main(String[] args) {
+        MyInt v= new MyInt();
+        v.value=10;
+        System.out.println("v="+v.value);
+        change(v);
+        System.out.println("v="+v.value);
+    }*/
+}
