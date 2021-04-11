893038e6cfccaf6ae6e6fff01b6f03dcecdbbe71
zvachniy <80152850+zvachniy@users.noreply.github.com>
2021-04-11 13:22:25 +0300

    Квадратное уравнение вычисляется и выводится верно. Проблема со вводом решена

--- labworks/labs/lab4.java
+++ labworks/labs/lab4.java
@@ -4,28 +4,33 @@ import java.util.Scanner;
 
 public class lab4 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);    //инициализируется для приблуды ввода консоли
+        Scanner scannerA = new Scanner(System.in);
+        Scanner scannerB = new Scanner(System.in);
+        Scanner scannerC = new Scanner(System.in);
         double a, b, c;
         double x1, x2;
         double Discriminant;
-
         System.out.println("Введите a, b, c для решения квадратного уравнения вида a*x^2+b*x+c=0");
-        System.out.println("Введите a: " + scanner);
-        a = scanner.nextDouble();
-        System.out.println("Введите b: " + scanner);
-        b = scanner.nextDouble();
-        System.out.println("Введите c: " + scanner);
-        c = scanner.nextDouble();
+        System.out.println("a, b и c могут вводиться вида 1 или -16 или 0,9787");
+        System.out.print("Введите a: ");
+        a = scannerA.nextDouble();
+        System.out.print("Введите b: ");
+        b = scannerB.nextDouble();
+        System.out.print("Введите c: ");
+        c = scannerC.nextDouble();
         Discriminant = b * b - 4 * a * c;
         if (Discriminant < 0)
-            System.out.println("Error");
+            System.out.println("Корней нет");
         else if (Discriminant > 0) {
-            x1 = (-b + Math.sqrt(Discriminant)) / (2 * a);
-            x2 = (-b - Math.sqrt(Discriminant)) / (2 * a);
-            System.out.println("x1= " + x1 + "  " + "x2= " + x2);
+            x2 = (-b + Math.sqrt(Discriminant)) / (2 * a);
+            x1 = (-b - Math.sqrt(Discriminant)) / (2 * a);
+            ///условие добавлено для красоты вывода
+            if (x1 <x2)
+                System.out.println("Решений два.    x1= " + x1 + "  " + "x2= " + x2);
+            else System.out.println("Решений два.    x1= " + x2 + "  " + "x2= " + x1);
         } else {
             x1 = -1 * (b / (2 * a));
-            System.out.println("x1=x2= " + x1);
+            System.out.println("Решение одно.   x1=x2= " + x1);
         }
     }
 }
