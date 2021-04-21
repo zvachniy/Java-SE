40fd7a063f80ee25c174f156097649fc6d276b64
zvachniy <80152850+zvachniy@users.noreply.github.com>
2021-04-21 22:30:14 +0300

    читабельность кода

--- labworks/labs/DragonWorld/Dragon.java
+++ labworks/labs/DragonWorld/Dragon.java
@@ -19,11 +19,12 @@ public class Dragon {
         this.tooth = tooth;
     }
 
-    public static void fire(Dragon firstDragon, Dragon secondDragon ) {
-        System.out.println("Дракон по имени " + firstDragon.getName() + " ударил дракона по имени " + secondDragon.getName()+ ". Нанесено " + firstDragon.getTooth()+ " урона");
+    public static void fire(Dragon firstDragon, Dragon secondDragon) {
+        System.out.println("Дракон по имени " + firstDragon.getName() + " ударил дракона по имени " + secondDragon.getName() + ". Нанесено " + firstDragon.getTooth() + " урона");
         System.out.println();
     }
-    public void ulta(){
+
+    public void ulta() {
         System.out.print("ОГОНЬ ***** вы горите.");
         System.out.println(" Это спецспособность всех Драконов");
         System.out.println();
@@ -34,24 +35,24 @@ public class Dragon {
 
         Dragon dragon1 = new Dragon("Анкалагон Пожиратель Гор", 100);
         Dragon dragon2 = new Dragon("Смауг Хитроумный", 50);
-        fire(dragon1,dragon2);
+        fire(dragon1, dragon2);
 
         //вызов наследников с их интерфейсами
         SilverWings dragon3 = new SilverWings("Среброкрыл", 150);
         dragon3.Fly();
-        fire(dragon3,dragon1);
+        fire(dragon3, dragon1);
         ShadowWhisper dragon4 = new ShadowWhisper("Думатель", 20);
         dragon4.MindControl();
-        fire(dragon4,dragon3);
-        TimeKeeper dragon5=new TimeKeeper("Хроми",75);
+        fire(dragon4, dragon3);
+        TimeKeeper dragon5 = new TimeKeeper("Хроми", 75);
         dragon5.Fly();
         dragon5.MindControl();
-        fire(dragon5,dragon4);
+        fire(dragon5, dragon4);
 
         //инициализация ещё одного наследника и прогон наследуемого метода через массив
-        Dragon dragonLast=new DeathWing("Смертокрыл",500);
-        Dragon[] arrayDragons=new Dragon[]{dragon1,dragon2,dragon3,dragon4,dragon5,dragonLast};
-        for (Dragon d:arrayDragons)
+        Dragon dragonLast = new DeathWing("Смертокрыл", 500);
+        Dragon[] arrayDragons = new Dragon[]{dragon1, dragon2, dragon3, dragon4, dragon5, dragonLast};
+        for (Dragon d : arrayDragons)
             d.ulta();
     }
 
