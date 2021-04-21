f6bd42d84670967fa808df73e89bb41b5afe5e3b
zvachniy <80152850+zvachniy@users.noreply.github.com>
2021-04-21 22:29:23 +0300

    Причёсывание. Выполнение всех трёх задач в одном классе

--- labworks/labs/DragonWorld/Dragon.java
+++ labworks/labs/DragonWorld/Dragon.java
@@ -20,8 +20,13 @@ public class Dragon {
     }
 
     public static void fire(Dragon firstDragon, Dragon secondDragon ) {
-        System.out.println("Дракон по имени " + firstDragon.getName() + " ударил дракона по имени " + secondDragon.getName() );
-        System.out.println("Нанесено " + firstDragon.getTooth()+ " урона");
+        System.out.println("Дракон по имени " + firstDragon.getName() + " ударил дракона по имени " + secondDragon.getName()+ ". Нанесено " + firstDragon.getTooth()+ " урона");
+        System.out.println();
+    }
+    public void ulta(){
+        System.out.print("ОГОНЬ ***** вы горите.");
+        System.out.println(" Это спецспособность всех Драконов");
+        System.out.println();
     }
 
     public static void main(String[] args) {
@@ -34,12 +39,20 @@ public class Dragon {
         //вызов наследников с их интерфейсами
         SilverWings dragon3 = new SilverWings("Среброкрыл", 150);
         dragon3.Fly();
+        fire(dragon3,dragon1);
         ShadowWhisper dragon4 = new ShadowWhisper("Думатель", 20);
         dragon4.MindControl();
+        fire(dragon4,dragon3);
         TimeKeeper dragon5=new TimeKeeper("Хроми",75);
         dragon5.Fly();
         dragon5.MindControl();
+        fire(dragon5,dragon4);
 
+        //инициализация ещё одного наследника и прогон наследуемого метода через массив
+        Dragon dragonLast=new DeathWing("Смертокрыл",500);
+        Dragon[] arrayDragons=new Dragon[]{dragon1,dragon2,dragon3,dragon4,dragon5,dragonLast};
+        for (Dragon d:arrayDragons)
+            d.ulta();
     }
 
 }
