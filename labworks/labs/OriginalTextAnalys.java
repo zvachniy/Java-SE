85446993da0a6e4ef9ebb325a9483b9419c2c7f1
zvachniy <d.kachalov@outlook.com>
2021-05-05 18:47:06 +0300

    добавление флагов, но он все равно неверно подсчитывает слова

--- labworks/labs/OriginalTextAnalys.java
+++ labworks/labs/OriginalTextAnalys.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class OriginalTextAnalys {
     private static String s1;   //строка ввода
 
+
     int countSymbols;
 
     public int getCountSymbols(String s) {
@@ -16,15 +17,42 @@ public class OriginalTextAnalys {
 
     int countLetters;   //количество букв в строке
 
-    public int getCountLetters(String s) {
-        int countOddLetters=0;
-        int sum=0;
+
+    public int makeMagic(String s) {
+        boolean flagComma = false;
+        boolean flagEndSentence = false;
+        boolean flagOddLetters = false;
+        //boolean flagLetters = true;
+        int countOddLetters = 0;
+        int sum = 0;
+        int Words = 0;
+        int pseudoWords=0;
         for (int i = 0; i < s.length(); i++) {
             sum++;
-            if (s.charAt(i) == ' ' | s.charAt(i) == ',' | s.charAt(i) == '.'| s.charAt(i) == '!') countOddLetters++;
+            if (s.charAt(i) == ' ' | s.charAt(i) == ',' | s.charAt(i) == '.' | s.charAt(i) == '!') {
+                flagOddLetters = true;
+                Words++;
+                countOddLetters++;
+            } /*else {
+                flagLetters = true;
+            }*/
+            if (s.charAt(i) == ',') flagComma = true;
+            if (s.charAt(i) == '.' | s.charAt(i) == '!') flagEndSentence = true;
+            if (flagComma & flagOddLetters) {
+                flagOddLetters = false;
+                flagComma = false;
+                pseudoWords++;
+            }
+            if (flagEndSentence && flagOddLetters) {
+                flagEndSentence = false;
+                flagOddLetters = false;
+                pseudoWords++;
+            }
         }
-        System.out.println("лишних символов "+countOddLetters);
-        countLetters=sum-countOddLetters;
+        Words=Words-pseudoWords;
+        System.out.println("лишних символов " + countOddLetters);
+        countLetters = sum - countOddLetters;
+        System.out.println("Количество слов в строке " + Words);
         return countLetters;
     }
 
@@ -32,10 +60,10 @@ public class OriginalTextAnalys {
         System.out.println("Введите текстовую строку");
         Scanner in = new Scanner(System.in);
         s1 = in.nextLine();
-        char[] chars = s1.toCharArray();
-        System.out.println(s1);
+        //char[] chars = s1.toCharArray();
+        //System.out.println(s1);
         OriginalTextAnalys n1 = new OriginalTextAnalys();
         System.out.println("Количество символов в строке " + n1.getCountSymbols(s1));
-        System.out.println("Количество букв в строке " + n1.getCountLetters(s1));
+        System.out.println("Количество букв в строке " + n1.makeMagic(s1));
     }
 }
